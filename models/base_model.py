615400b11b925792b72c504a5850ce700c579440
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-08-09 00:49:25 +0300

    Updating BaseModel Class to take **kwargs .

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,22 @@ from datetime import datetime
 class BaseModel:
     """The Parent Class For This Project
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ The Class Constructor """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """ Print a Readable String"""
