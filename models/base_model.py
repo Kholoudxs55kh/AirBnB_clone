8c8e1e733579a78c2141d2813a822f0ecc298c57
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-08-09 04:59:39 +0300

    Calling The New() and Save() Methods to Store OurData

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Class "BaseModel" For The Project
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -25,6 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """ Print a Readable String"""
@@ -34,7 +36,7 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute updated_at
         with the current datetime"""
-
+        models.storage.save()
         self.updated_at = datetime.now()
 
     def to_dict(self):
