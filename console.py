270abca0ca67459790ad733c9a0ce8a0623ea6dc
Momen Morgan <117030094+MomenMorgan@users.noreply.github.com>
2023-08-10 03:58:41 +0300

    Adding The Destroy And All Methods.

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 entry point of the command interpreter"""
 import cmd
 from models.base_model import BaseModel
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -46,14 +47,68 @@ class HBNBCommand(cmd.Cmd):
 
         print(className.id)
 
-    def do_show(self):
-        pass
+    def do_show(self, line):
+        """Prints the string representation of an instance"""
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+        args = line.split(" ")
 
-    def do_destroy(self):
-        pass
+        try:
+            if args[0] not in self.Classes_Name:
+                print("** class doesn't exist **")
+        except NameError:
+            pass
 
-    def do_all(self):
-        pass
+        if len(line) == 1:
+            print("** instance id missing **")
+
+        all_ = models.storage.all()
+
+        id_ = args[0] + "." + args[1]
+        if id_ in all_:
+            print(all_[id_])
+        else:
+            print('** no instance found **')
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+        args = line.split(" ")
+
+        try:
+            if args[0] not in self.Classes_Name:
+                print("** class doesn't exist **")
+        except NameError:
+            pass
+
+        if len(line) == 1:
+            print("** instance id missing **")
+
+        all_ = models.storage.all()
+        id_ = args[0] + "." + args[1]
+        if id_ in all_:
+            del (all_[id_])
+            models.storage.save()
+        else:
+            print('** no instance found **')
+
+    def do_all(self, line):
+        """Prints all string representation of all instances"""
+        all_ = models.storage.all()
+        # args = line.split(" ")
+        if not line:
+            for value in all_.values():
+                print([str(value)])
+        elif line:
+            if line in self.Classes_Name:
+                for value in all_.values():
+                    if value.__class__.__name__ == line:
+                        print([str(value)])
+            else:
+                print("** class doesn't exist **")
 
     def do_update(self):
         pass
