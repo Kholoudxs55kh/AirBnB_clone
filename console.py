f63531269f190b4e16e4156b1913fbe6bbb585ed
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-08-10 05:49:50 +0300

    Fixing Output's Errors

--- console.py
+++ console.py
@@ -18,7 +18,7 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
     Classes_Name = ['BaseModel', 'User', 'City', 'State'
-                   'Amenity', 'Place', 'Review']
+                    'Amenity', 'Place', 'Review']
 
     def do_quit(self, line):
         """Quits The Program
@@ -46,6 +46,7 @@ class HBNBCommand(cmd.Cmd):
         try:
             if args[0] not in self.Classes_Name:
                 print("** class doesn't exist **")
+                return
         except NameError:
             pass
         # now in the 1st arg we have the Class Name,
@@ -65,11 +66,13 @@ class HBNBCommand(cmd.Cmd):
         try:
             if args[0] not in self.Classes_Name:
                 print("** class doesn't exist **")
+                return
         except NameError:
             pass
 
-        if len(line) == 1:
+        if len(args) == 1:
             print("** instance id missing **")
+            return
 
         all_ = models.storage.all()
 
@@ -78,12 +81,50 @@ class HBNBCommand(cmd.Cmd):
             print(all_[id_])
         else:
             print('** no instance found **')
+            return
 
-    def do_destroy(self):
-        pass
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+        args = line.split(" ")
 
-    def do_all(self):
-        pass
+        try:
+            if args[0] not in self.Classes_Name:
+                print("** class doesn't exist **")
+                return
+        except NameError:
+            pass
+
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+
+        all_ = models.storage.all()
+        id_ = args[0] + "." + args[1]
+        if id_ in all_:
+            del (all_[id_])
+            models.storage.save()
+        else:
+            print('** no instance found **')
+            return
+
+    def do_all(self, line):
+        """Prints all string representation of all instances"""
+        all_ = models.storage.all()
+        # args = line.split(" ")
+        if not line:
+            for value in all_.values():
+                print([str(value)])
+        elif line:
+            if line in self.Classes_Name:
+                for value in all_.values():
+                    if value.__class__.__name__ == line:
+                        print([str(value)])
+            else:
+                print("** class doesn't exist **")
+                return
 
     def do_update(self):
         pass
